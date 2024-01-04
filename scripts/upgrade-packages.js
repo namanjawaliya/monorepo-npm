d01c546b6d42e158c555f33e065c1697b706f286
Naman Jawaliya <jawaliyan@gmail.com>
2024-01-04 22:01:05 +0530

    upgrade-packages script

---
+++ scripts/upgrade-packages.js
@@ -0,0 +1,47 @@
+const { execSync } = require("child_process");
+
+const workspaces = JSON.parse(execSync(`npm query .workspace`).toString());
+
+const versionInput = [...process.argv][2];
+
+workspaces
+  .filter(({ name }) => name.startsWith("@rd/"))
+  .forEach((workspace) => {
+    const newVersion = execSync(
+      `npm version ${versionInput ? versionInput : "patch"} --workspace ${
+        workspace.name
+      } --no-workspaces-update`
+    )
+      .toString()
+      .split("\n")[1]
+      .slice(1);
+
+    console.log("workspace", workspace.name);
+    console.log("\t old version", workspace.version);
+    console.log("\t new version", newVersion);
+
+    const userWorkspaces = workspaces.filter(
+      ({ dependencies }) => dependencies?.[workspace.name]
+    );
+
+    userWorkspaces.forEach((userWorkspace) => {
+      if (userWorkspace.dependencies?.[workspace.name] !== "*") {
+        execSync(
+          `npm pkg set dependencies.${workspace.name}=${newVersion} --workspace ${userWorkspace.name}`
+        );
+
+        console.log("\t updated user workspace", userWorkspace.name);
+      }
+    });
+  });
+
+execSync("npm install");
+
+// publish
+// need auth This command requires you to be logged in to https://registry.npmjs.org/
+// need auth You need to authorize your machine using `npm adduser`
+// workspaces
+//   .filter(({ name }) => name.startsWith("@rd/"))
+//   .forEach((workspace) => {
+//     execSync(`npm publish --workspace ${workspace.name}`);
+//   });
